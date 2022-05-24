caa9576a70e12c0f8f3a677a27c553508a00d9c0
trance <michaelmunavu83@gmail.com>
2022-05-24 09:36:53 +0300

    Add functionality to delete and styles

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const add = document.getElementById('add');
 const awesomeBooks = [];
-add.addEventListener('click', () => {
+add.addEventListener('click', (e) => {
+  e.preventDefault();
   const title = document.getElementById('title');
   const author = document.getElementById('author');
 
@@ -13,23 +14,30 @@ add.addEventListener('click', () => {
 
   localStorage.setItem('books', JSON.stringify(awesomeBooks));
 
-  if (title === '' || author === '') {
-    alert('tcuvybu');
-  } else {
-    const list = document.getElementById('book-list');
+  const list = document.getElementById('book-list');
 
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('book');
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('book');
 
-    newDiv.innerHTML += `
+  newDiv.innerHTML += `
         <p>${book.Title}</p>
         <p>${book.Author}</p>
       <button class="delete">Delete</button>      
       `;
 
-    list.appendChild(newDiv);
+  list.appendChild(newDiv);
 
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+});
+document.getElementById('book-list').addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
   }
+  awesomeBooks.forEach((book, i) => {
+    if (e.target.parentElement.firstElementChild.textContent === book.Title) {
+      awesomeBooks.splice(i, 1);
+    }
+  });
+  localStorage.setItem('books', JSON.stringify(awesomeBooks));
 });
