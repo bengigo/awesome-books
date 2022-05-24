ee056491f0eac36817d6ad278182b541026f9c86
trance <michaelmunavu83@gmail.com>
2022-05-24 17:36:42 +0300

    Add html and classes for Javascript

--- script.js
+++ script.js
@@ -1,57 +1,67 @@
-const add = document.getElementById('add');
-const awesomeBooks = [];
-
-add.addEventListener('click', (e) => {
-  e.preventDefault();
-  const title = document.getElementById('title');
-  const author = document.getElementById('author');
-
-  const book = {
-    Title: title.value,
-    Author: author.value,
-  };
-  const list = document.getElementById('book-list');
-  const newDiv = document.createElement('div');
-  newDiv.classList.add('book');
-
-  awesomeBooks.push(book);
-
-  localStorage.setItem('books', JSON.stringify(awesomeBooks));
-
-  newDiv.innerHTML += `
-        <p>${book.Title}</p>
-        <p>${book.Author}</p>
-      <button class="delete">Delete</button>      
-      `;
-
-  list.appendChild(newDiv);
-
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-});
-document.getElementById('book-list').addEventListener('click', (e) => {
-  e.preventDefault();
-  if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
+class Books {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-  awesomeBooks.forEach((book, i) => {
-    if (e.target.parentElement.firstElementChild.textContent === book.Title) {
-      awesomeBooks.splice(i, 1);
-    }
-  });
-  localStorage.setItem('books', JSON.stringify(awesomeBooks));
-});
-window.addEventListener('load', () => {
-  const stores = JSON.parse(localStorage.getItem('books'));
-  stores.forEach((store) => {
-    const newDiv = document.createElement('div');
-    newDiv.innerHTML += `
-    <p>${store.Title}</p>
-    <p>${store.Author}</p>
-  <button class="delete">Delete</button>      
-  `;
+}
+
+const StoredBooks = [];
+
+class Display {
+  static addtolist() {
+    const Title = document.getElementById('title').value;
+    const Author = document.getElementById('author').value;
     const list = document.getElementById('book-list');
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('book');
+    const newbook = new Books(Title, Author);
+    StoredBooks.push(newbook);
+
+    newDiv.innerHTML += `
+    <span>" ${newbook.title} " </span><span> by</span><span> ${newbook.author}</span>
+      <button class="delete">Delete</button>
+    `;
 
     list.appendChild(newDiv);
-  });
-});
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+  }
+
+  static addtolocal() {
+    localStorage.setItem('books', JSON.stringify(StoredBooks));
+  }
+
+  static delBook(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('delete')) {
+      e.target.parentElement.remove();
+    }
+  }
+
+  static removelocal(e) {
+    StoredBooks.forEach((newbook, i) => {
+      if (
+        e.target.parentElement.firstElementChild.textContent === newbook.title
+      ) {
+        StoredBooks.splice(i, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(StoredBooks));
+  }
+
+  static preservelocal() {
+    const stores = JSON.parse(localStorage.getItem('books'));
+    stores.forEach((store) => {
+      const newDiv = document.createElement('div');
+      newDiv.classList.add('book');
+      newDiv.innerHTML += `
+    <span>" ${store.title} " </span><span> by</span><span> ${store.author}</span>
+   
+    <button class="delete">Delete</button>
+  `;
+      const list = document.getElementById('book-list');
+
+      list.appendChild(newDiv);
+    });
+  }
+}
