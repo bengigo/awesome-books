884fcc8c20ac601b8d92fa091e2d1dc9415745f3
bengigenc <genc.bengi@gmail.com>
2022-05-25 17:31:30 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -8,21 +8,17 @@ class Display {
   }
 
   static addtolist() {
-    const Title = document.getElementById("title").value;
-    const Author = document.getElementById("author").value;
+    const Title = document.getElementById('title').value;
+    const Author = document.getElementById('author').value;
 
-    if (Title == "" || Author == "") {
-      document.querySelector('.alert').innerHTML='Please add a title and an author'
-
-
-      
+    if (Title === '' || Author === '') {
+      document.querySelector('.alert').innerHTML = 'Please add a title and an author';
     } else {
-      document.querySelector('.alert').innerHTML = ''
+      document.querySelector('.alert').innerHTML = '';
 
-
-      const list = document.getElementById("book-list");
-      const newDiv = document.createElement("div");
-      newDiv.classList.add("book");
+      const list = document.getElementById('book-list');
+      const newDiv = document.createElement('div');
+      newDiv.classList.add('book');
       const newbook = new Display(Title, Author);
       StoredBooks.push(newbook);
 
@@ -32,18 +28,18 @@ class Display {
     `;
 
       list.appendChild(newDiv);
-      document.getElementById("title").value = "";
-      document.getElementById("author").value = "";
+      document.getElementById('title').value = '';
+      document.getElementById('author').value = '';
     }
   }
 
   static addtolocal() {
-    localStorage.setItem("books", JSON.stringify(StoredBooks));
+    localStorage.setItem('books', JSON.stringify(StoredBooks));
   }
 
   static delBook(e) {
     e.preventDefault();
-    if (e.target.innerHTML === "Delete") {
+    if (e.target.innerHTML === 'Delete') {
       e.target.parentElement.remove();
     }
   }
@@ -56,40 +52,40 @@ class Display {
         StoredBooks.splice(i, 1);
       }
     });
-    localStorage.setItem("books", JSON.stringify(StoredBooks));
+    localStorage.setItem('books', JSON.stringify(StoredBooks));
   }
 
   static preservelocal() {
-    const stores = JSON.parse(localStorage.getItem("books"));
+    const stores = JSON.parse(localStorage.getItem('books'));
     stores.forEach((store) => {
-      const newDiv = document.createElement("div");
-      newDiv.classList.add("book");
+      const newDiv = document.createElement('div');
+      newDiv.classList.add('book');
       newDiv.innerHTML += `
       <div class="info">
       <p class="line">"${store.title}" by ${store.author}</p>
       </div>
       <button id="${store.id}" class="delete">Delete</button>
     `;
-      const list = document.getElementById("book-list");
+      const list = document.getElementById('book-list');
 
       list.appendChild(newDiv);
     });
   }
 }
 
-const add = document.getElementById("add");
+const add = document.getElementById('add');
 
-add.addEventListener("click", (e) => {
+add.addEventListener('click', (e) => {
   e.preventDefault();
   Display.addtolist();
   Display.addtolocal();
 });
-document.getElementById("book-list").addEventListener("click", (e) => {
+document.getElementById('book-list').addEventListener('click', (e) => {
   e.preventDefault();
   Display.delBook(e);
   Display.removelocal(e);
 });
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   Display.preservelocal();
 });
